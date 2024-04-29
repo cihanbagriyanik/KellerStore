68e5e2e882c88c7fe0e8b41b4a1174bbba017bab
cihanbagriyanik <cihanbagriyanikde@gmail.com>
2024-04-29 14:18:43 +0200

    Create a Category model according to ERD. Create also an initial Category controller and Category routes.

--- Backend/src/models/category.js
+++ Backend/src/models/category.js
@@ -7,13 +7,30 @@ const { mongoose } = require("../configs/dbConnection");
 
 /* -------------------------------------------------------------------------- */
 // {
-
+//     "topCategoryId": "000000000"
+//     "categoryId": "000000000"
+//     "name": "category-1",
 // }
 /* -------------------------------------------------------------------------- */
 //? Category Model:
 const CategorySchema = new mongoose.Schema(
   {
-   
+    topCategoryId: {
+      type: Number,
+      required: true,
+      trim: true,
+    },
+    categoryId: {
+      type: Number,
+      required: true,
+      trim: true,
+    },
+    name: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true,
+    },
   },
   {
     collection: "categories",
---
+++ Backend/src/routes/category.js
@@ -0,0 +1,24 @@
+"use strict";
+/* --------------------------------------------------------------------------
+    * NODEJS EXPRESS | Keller Store
+----------------------------------------------------------------------------- */
+//? Requaring
+const router = require("express").Router();
+
+const category = require("../controllers/category");
+
+const permissions = require("../middlewares/permissions");
+
+/* -------------------------------------------------------------------------- */
+//! URL: /category
+router.route("/").get(category.list).post(category.create);
+
+router
+  .route("/:id")
+  .get(category.read)
+  .put(category.update)
+  .patch(category.update)
+  .delete(category.delete);
+
+/* -------------------------------------------------------------------------- */
+module.exports = router;
