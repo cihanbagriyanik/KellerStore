0634dddf82a36fe5273250fcc004532b880aa378
hasankocde <hasankocoglu21@gmail.com>
2024-04-29 15:09:26 +0200

    User (model, controller, routes) created based on ERD

--- Backend/src/controllers/user.js
+++ Backend/src/controllers/user.js
@@ -1,132 +1,66 @@
 "use strict";
-/* --------------------------------------------------------------------------
-    * NODEJS EXPRESS | Keller Store
------------------------------------------------------------------------------ */
-//? Requaring
 const User = require("../models/user");
-const sendMail = require("../helpers/sendMail");
+const Token = require("../models/token");
+const passwordEncrypt = require("../helpers/passwordEncrypt");
 
-/* -------------------------------------------------------------------------- */
-//? User Controller:
 module.exports = {
-  //! GET
   list: async (req, res) => {
-    /*
-        #swagger.tags = ["Users"]
-        #swagger.summary = "List Users"
-        #swagger.description = `
-            You can send query with endpoint for search[], sort[], page and limit.
-            <ul> Examples:
-                <li>URL/?<b>filter[field1]=value1&filter[field2]=value2</b></li>
-                <li>URL/?<b>search[field1]=value1&search[field2]=value2</b></li>
-                <li>URL/?<b>sort[field1]=1&sort[field2]=-1</b></li>
-                <li>URL/?<b>page=2&limit=1</b></li>
-            </ul>
-        `
-    */
-
-    const data = await res.getModelList(User);
-
+    const filters = req.user?.isAdmin ? {} : { _id: req.user._id };
+    const data = await res.getModelList(User, filters);
     res.status(200).send({
       error: false,
-      details: await res.getModelListDetails(User),
+      details: await res.getModelListDetails(User, filters),
       data,
     });
   },
 
-  //* CRUD Processes:
-  //! POST
   create: async (req, res) => {
-    /*
-        #swagger.tags = ["Users"]
-        #swagger.summary = "Create User"
-        #swagger.parameters['body'] = {
-                in: 'body',
-                required: true,
-                schema: {
-                    "username": "test",
-                    "password": "1234",
-                    "email": "test@site.com",
-                    "isActive": true,
-                    "isStaff": false,
-                    "isAdmin": false,
-                }
-            }
-    */
+    req.body.isActive = true;
+    req.body.isBusiness = req.body.isBusiness || false;
+    req.body.isPremium = req.body.isPremium || false;
     const data = await User.create(req.body);
-
-    sendMail(
-      // to:
-      data.email,
-      // subject:
-      "Welcome",
-      // Message:
-      `
-          <h1>Welcome to Keller Store</h1>
-          <p>Dear <b>${data.username}</b>, you can order your Keller Store now!</p>
-      `
-    );
-
+    const tokenData = await Token.create({
+      userId: data._id,
+      token: passwordEncrypt(data._id + Date.now()),
+    });
     res.status(201).send({
       error: false,
+      token: tokenData.token,
       data,
     });
   },
 
-  //! /:id -> GET
   read: async (req, res) => {
-    /*
-        #swagger.tags = ["Users"]
-        #swagger.summary = "Get Single User"
-    */
-
-    const data = await User.findOne({ _id: req.params.id });
-
+    const filters = req.user?.isAdmin
+      ? { _id: req.params.id }
+      : { _id: req.user._id };
+    const data = await User.findOne(filters);
     res.status(200).send({
       error: false,
       data,
     });
   },
 
-  //! /:id -> PUT / PATCH
   update: async (req, res) => {
-    /*
-        #swagger.tags = ["Users"]
-        #swagger.summary = "Update User"
-        #swagger.parameters['body'] = {
-                in: 'body',
-                required: true,
-                schema: {
-                    "username": "test",
-                    "password": "1234",
-                    "email": "test@site.com",
-                    "isActive": true,
-                    "isStaff": false,
-                    "isAdmin": false,
-                }
-            }
-    */
-
-    const data = await User.updateOne({ _id: req.params.id }, req.body, {
+    const filters = req.user?.isAdmin
+      ? { _id: req.params.id }
+      : { _id: req.user._id };
+    req.body.isAdmin = req.user?.isAdmin ? req.body.isAdmin : false;
+    const data = await User.updateOne(filters, req.body, {
       runValidators: true,
     });
-
     res.status(202).send({
       error: false,
       data,
-      new: await User.findOne({ _id: req.params.id }),
+      new: await User.findOne(filters)
     });
   },
 
-  //! /:id -> DELETE
   delete: async (req, res) => {
-    /*
-        #swagger.tags = ["Users"]
-        #swagger.summary = "Delete User"
-    */
-
-    const data = await User.deleteOne({ _id: req.params.id });
-
+    const filters = req.user?.isAdmin
+      ? { _id: req.params.id }
+      : { _id: req.user._id };
+    const data = await User.deleteOne(filters);
     res.status(data.deletedCount ? 204 : 404).send({
       error: !data.deletedCount,
       data,
--- Backend/src/models/user.js
+++ Backend/src/models/user.js
@@ -1,58 +1,159 @@
-"use strict";
-/* --------------------------------------------------------------------------
-    * NODEJS EXPRESS | Keller Store
------------------------------------------------------------------------------ */
-//? Requaring
-const { mongoose } = require("../configs/dbConnection");
+"use strict"
 
-const passwordEncrypt = require("../helpers/passwordEncrypt");
-/* -------------------------------------------------------------------------- */
+const { mongoose } = require("../configs/dbConnection")
+
+/* -------------------------------------------------------------------------- 
 // {
-//     "username": "test",
-//     "password": "1234",
-//     "email": "test@site.com",
-//     "isAdmin": "true"
+//         "firstName": "user3",
+    "lastName": "user3",
+    "userName": "user3",
+    "businessName": "1234567877",
+    "email": "user3@example.com",
+    "password": "Secure*1234",
+    "dateOfBirth": "1982-01-01",
+    "tel": "1234567897",
+    "isActive": true,
+    "isAdmin": false,
+    "isBusiness": false,
+    "isPremium": false,
+    "taxNr": 12345675541,
+    "startDate": "2021-01-01",
+    "endDate": "2025-01-01",
+    "future": "Some future info3"
 // }
-/* -------------------------------------------------------------------------- */
-//? User Model:
-const UserSchema = new mongoose.Schema(
-  {
-    username: {
-      type: String,
-      trim: true,
-      required: true,
-      unique: true,
-    },
+-------------------------------------------------------------------------- */
 
-    password: {
-      type: String,
-      trim: true,
-      required: true,
-      set: (password) => passwordEncrypt(password),
-    },
 
+
+// User Model:
+
+const UserSchema = new mongoose.Schema({
+    firstName: {
+        type: String,
+        trim: true,
+        required: true
+    },
+    lastName: {
+        type: String,
+        trim: true,
+        required: true
+    },
+    userName: {
+        type: String,
+        trim: true,
+        required: true,
+        unique: true,
+        index: true
+    },
+    businessName: {
+        type: String,
+        required: true
+    },
     email: {
-      type: String,
-      trim: true,
-      required: true,
-      unique: true,
+        type: String,
+        trim: true,
+        required: true,
+        unique: true,
+        index: true
     },
-
+    password: {
+        type: String,
+        trim: true,
+        required: true
+    },
+    avatar: {
+        type: String,
+        trim: true
+    },
+    dateOfBirth: {
+        type: Date,
+        required: true
+    },
+    tel: {
+        type: Number,
+        required: true
+    },
     isActive: {
-      type: Boolean,
-      default: true,
+        type: Boolean,
+        default: true
     },
-//hasan
-    isAdmin: {
-      type: Boolean,
-      default: false,
+    isBusiness: {
+        type: Boolean,
+        default: false
     },
-  },
-  {
-    collection: "users",
-    timestamps: true,
-  }
-);
+    isPremium: {
+        type: Boolean,
+        default: false
+    },
+    taxNr: {
+        type: Number,
+        required: true,
+        unique: true
+    },
+    isStaff: {
+        type: Boolean,
+        default: false
+    },
 
-/* -------------------------------------------------------------------------- */
-module.exports = mongoose.model("User", UserSchema);
+    isAdmin: {
+        type: Boolean,
+        default: false
+    },
+    startDate: {
+        type: Date,
+        required: true
+    },
+    endDate: {
+        type: Date,
+        required: true
+    },
+    future: {
+        type: String,
+        required: true
+    }
+}, { collection: 'users', timestamps: true })
+
+
+/* ------------------------------------------------------- */
+// Schema Configs:
+
+const passwordEncrypt = require('../helpers/passwordEncrypt')
+
+UserSchema.pre(['save', 'updateOne'], function (next) {
+
+    // get data from "this" when create;
+    // if process is updateOne, data will receive in "this._update"
+    const data = this?._update || this
+
+    // email@domain.com
+    const isEmailValidated = data.email
+        ? /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(data.email) // test from "data".
+        : true
+
+    if (isEmailValidated) {
+
+        if (data?.password) {
+
+            // pass == (min 1: lowerCase, upperCase, Numeric, @$!%*?& + min 8 chars)
+            const isPasswordValidated = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&]).{8,}$/.test(data.password)
+
+            if (isPasswordValidated) {
+
+                this.password = data.password = passwordEncrypt(data.password)
+                this._update = data // updateOne will wait data from "this._update".
+
+            } else {
+
+                next(new Error('Password not validated.'))
+            }
+        }
+
+        next() // Allow to save.
+
+    } else {
+
+        next(new Error('Email not validated.'))
+    }
+})
+/* ------------------------------------------------------- */
+module.exports = mongoose.model('User', UserSchema)
