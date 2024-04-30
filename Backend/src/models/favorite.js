a796657394855e24353d3ff72370427dadb6381e
EsraYalcin <yalcinaceresra@gmail.com>
2024-04-30 09:46:50 +0200

    esra-Favorite (model, controller, routes)

--- Backend/src/models/favorite.js
+++ Backend/src/models/favorite.js
@@ -3,23 +3,100 @@
     * NODEJS EXPRESS | Keller Store
 ----------------------------------------------------------------------------- */
 //? Requaring
-const { mongoose } = require("../configs/dbConnection");
-
+const Favorite=require("../models/favorite")
 /* -------------------------------------------------------------------------- */
-// {
+//? Favorite Controller:
+module.exports = {
+  //! GET
+  list: async (req, res) => {
+    /*
+  #swagger.tags = ["Favorite"]
+  #swagger.summary = "List Favorite"
+  #swagger.description = `
+      You can send query with endpoint for search[], sort[], page and limit.
+      <ul> Examples:
+          <li>URL/?<b>search[field1]=value1&search[field2]=value2</b></li>
+          <li>URL/?<b>sort[field1]=1&sort[field2]=-1</b></li>
+          <li>URL/?<b>page=2&limit=1</b></li>
+      </ul>
+  `
+*/
+//* CRUD Processes:
 
-// }
+const data = await res.getModelList(Favorite, filters);
 
-/* -------------------------------------------------------------------------- */
-//? Favorite Model:
-const FavoriteSchema = new mongoose.Schema(
-  {},
-
-  {
-    collection: "favorites",
-    timestamps: true,
+res.status(200).send({
+error: false,
+details: await res.getModelListDetails(Favorite, filters),
+data,
+});},
+ 
+  //! POST
+  create: async (req, res) => { /*
+  #swagger.tags = ["Favorites"]
+  #swagger.summary = "Create Favorite"
+  #swagger.parameters['body'] = {
+      in: 'body',
+      required: true,
+      schema: {
+      }
   }
-);
+*/
+const data = await Favorite.create(req.body);
 
-/* -------------------------------------------------------------------------- */
-module.exports = mongoose.model("Favorite", FavoriteSchema);
+    res.status(201).send({
+      error: false,
+      data,
+    });
+},
+
+  //! /:id -> GET
+  read: async (req, res) => {
+     /*
+  #swagger.tags = ["Favorites"]
+  #swagger.summary = "Get Single Favorite"
+*/
+const data = await Favorite.findOne({ _id: req.params.id });
+
+    res.status(200).send({
+      error: false,
+      data,
+    });
+},
+
+  //! /:id -> PUT / PATCH
+  update: async (req, res) => {
+   /*
+            #swagger.tags = ["Favorites"]
+            #swagger.summary = "Update Favorite"
+            #swagger.parameters['body'] = {
+                in: 'body',
+                required: true,
+                schema: {
+                }
+            }
+        */
+            const data = await Favorite.updateOne({ _id: req.params.id }, req.body, {
+              runValidators: true,
+            });
+        
+            res.status(200).send({
+              error: false,
+              data,
+              new: await Favorite.findOne({ _id: req.params.id }),
+            });
+},
+
+  //! /:id -> DELETE
+  delete: async (req, res) => {/*
+  #swagger.tags = ["Favorites"]
+  #swagger.summary = "Delete Favorite"
+*/
+
+const data = await Favorite.deleteOne({ _id: req.params.id });
+
+    res.status(data.deletedCount ? 204 : 404).send({
+      error: !data.deletedCount,
+      data,
+    });},
+};
