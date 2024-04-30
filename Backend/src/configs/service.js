9ace101b28ea84897adad00dc3695017c3c2dd85
Muhammet <msolmaz83@gmail.com>
2024-04-30 10:18:47 +0200

    connect

--- Backend/src/configs/service.js
+++ Backend/src/configs/service.js
@@ -1,14 +1,15 @@
 const service ={
     "type": "service_account",
     "project_id": "kellerstore-com",
-    "private_key_id": process.env.PRIVATE_KEY_ID,
-    "private_key": process.env.PRIVATE_KEY,
+    "private_key_id": "213198afa3183c9707e95ceb1050c0c4c625976d",
+    "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQCxeBvA+v52bpIT\nnCJfRPdLGLA3dcqqtZ7pUqm98O73MHeCGgZMW4rr6QDS4s+koiISEomgR6sbcDsc\nufXV3xzo1L3qlboUw1ZmfK3MhEhGq7JyPnCv/f0Q9sXTUPYbAdhSxZMkxzk4gMVa\nA/Bi+6l/LUT1vXlsDmfHaFcWzFr0qh63391Od50zmWVJaKJ8pDs/7mxCpYih/0lL\n7jXPZQ8UjCPMQ/JqWuxLpjpXGe/f8DQDoN4fC4ZuzlC0DVq58glepJuY1Eqr5Bp2\nqnJxoLRr32d2EKrstU3OQtc7f9Azceer9RzLCZT4no2GSJpG2QKujBpCljCw2n2E\nXa97x8CBAgMBAAECggEAA2YNFyiFFbqKyTARfnIBKz0ScSMsIu03WTlCiscq7YiF\nAobmoh64G5MvZRFbVH8E89+BuV1s2vLhEBGOxHdoKGWcT3pFSoPJuQO7hlaAMUUi\n7KWZVLjrs7JAbFOCYekGEFBOdrnq2UHOoSl3uI6y0X6cOtTTu4f0hYxQEF3HXiSG\n3jY55k0MrjSmyB2mEgzXY60aCBD4juZIZEfjaSivGQpdNuiYNOekSuJRHn1DyiD6\nSfSxNuDyAH2E/1MPP8DC2CzhhJ/rQ1BcYMVoBR6NHSZ+zOvdidZ3uWPc2dLwjRpb\nhdjByRpCh9sO/ZKn+pVIPceydbQw+6CuSRiI9PAlKwKBgQDaWDMgDP2Q27Efw9pe\n9KnZg7EqivQNJb3mFm4abIaG2zc4b7+/povVuDTyxTEu1IH0wEDZlJZTbzmiJot2\n5tlMieMHRJkDuJgdRuwpL/v/5UeyY0L94SAI3DevkiTZIVKIHbh3PwkVMGt1poT9\nI9S1UbcU4xirxinI0GDo+EJfhwKBgQDQE0hNx389HRS5vvz34AZTlbRzaKsEa8fZ\nE9ibRG3MrwvQ66Rb1hhrc83w0bfNVXY7wD08H5oZT5tBVATtYG7Swar8h1bowUik\ngSDrmUOf//4CcJFaHj7NLzP5HiSlgJPjg2X/OK87jw31OvBjyUndDGvWveYTRKcF\nM4G9fHaRtwKBgQC4oOYyTunHKQ4gdyNbMfXB2Srhd3gg+e4W9paJv4UN2aZbEZuJ\nupH52XbkZjxNhiKRzFPJ6GwpBx8r7v31QAU1RE92INpmnBv8fbFKrqmfW8iBno/S\nUCAffd+UlbaU6Cl6se7+3o2WnkfvxywjRb4iyINQpSuCbOF+Tj30IaE2VwKBgEPn\nxxT7Il9DelwUJGKQzf/BP57abdWHhzUk8O/bKSo/hk+sV3tRJ68JoBAToTR1y5w8\nZL+WYKgZ/sUXMnR1oDVP1wmK8aYMTjTBn4jD//nbhX4uVoFelYhI+2E1oSgSCjGZ\n1FOyoB2D1j5N+d3W9zAYuajfWJK5OPjNkm3QjLGJAoGAVzU2hYYawRTds1S3X8Pd\nsCGqeaJBOvcIWu8jROWl3sZyeDNYhC1Lrh0ZR2M6iEj6caP1i2ZgJUtdAQyUumvl\n/D3OS7U+DEsxCVbBHkjnhYJbBhdoBkrj8Kc5wjQBreoCGHF/Xgm4YumlafjSkKlr\njMxuj/+1TBN107P+eAcXX0Y=\n-----END PRIVATE KEY-----\n",
     "client_email": "firebase-adminsdk-bcq1t@kellerstore-com.iam.gserviceaccount.com",
-    "client_id": process.env.CLIENT_ID,
+    "client_id": "106895980809767873511",
     "auth_uri": "https://accounts.google.com/o/oauth2/auth",
     "token_uri": "https://oauth2.googleapis.com/token",
     "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
     "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-bcq1t%40kellerstore-com.iam.gserviceaccount.com",
     "universe_domain": "googleapis.com"
+    
   }
   module.exports = service
