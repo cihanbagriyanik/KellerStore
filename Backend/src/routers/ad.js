1fbe3fc748ddc1c4c1266f6afbb2dac2aa122b7d
Muhammet <msolmaz83@gmail.com>
2024-05-03 12:28:28 +0200

    ad_upload_route

--- Backend/src/routers/ad.js
+++ Backend/src/routers/ad.js
@@ -8,12 +8,12 @@ const router = require("express").Router();
 const ad = require("../controllers/ad");
 
 const permissions = require("../middlewares/permissions");
-
+const upload = require("../middlewares/upload")
 /* -------------------------------------------------------------------------- */
 //! URL: /ad
 router.route("/")
 .get(ad.list)
-.post(permissions.isLogin,ad.create);
+.post(permissions.isLogin,upload.array("ad",5)  ,ad.create);
 
 router
   .route("/:id")
