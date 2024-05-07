1088cbf7d724822fef4b303ab88e5f8da164efa4
cihanbagriyanik <cihanbagriyanikde@gmail.com>
2024-05-07 11:23:56 +0200

    navbar and some button and settings update

--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -1,9 +1,63 @@
-import React from 'react'
+import React from "react";
+import Logo from "../assets/logo.png";
+import NewAdButton from "./buttons/NewAdButton";
+import RegisterButton from "./buttons/RegisterButton";
+import LoginButton from "./buttons/LoginButton";
+import MessageIcon from "./icons/MessageIcon";
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <nav className="bg-white border-b">
+      <div className="flex items-center space-x-8 py-3 px-4 max-w-screen-xl mx-auto md:px-8">
+        <div className="flex-none lg:flex-initial">
+          <a href="/">
+            <img src={Logo} width={120} height={50} alt="Logos" />
+          </a>
+        </div>
 
-export default Navbar
+        <div>
+          <NewAdButton />
+        </div>
+
+        <div className="flex-1 flex items-center  sm:space-x-6 ">
+          <form className="flex items-center space-x-2 border rounded-md p-2 bg-gray-100">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5 flex-none text-gray-500  "
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+              />
+            </svg>
+            <input
+              className=" w-full outline-none appearance-none placeholder-gray-500 text-gray-500 sm:w-auto bg-gray-100 "
+              type="text"
+              placeholder="Suchen Sie, was immer Sie brauchen"
+              style={{ paddingRight: "2rem", width: "23rem" }}
+            />
+          </form>
+        </div>
+
+        <div>
+          <MessageIcon count={5} />
+        </div>
+
+        <div>
+          <RegisterButton />
+        </div>
+
+        <div>
+          <LoginButton />
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
