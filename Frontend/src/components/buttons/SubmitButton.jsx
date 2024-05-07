e74ec3bb5065e3b22c197b386d4a31dca9f3040d
cihanbagriyanik <cihanbagriyanikde@gmail.com>
2024-05-07 16:18:18 +0200

    SubmitButton for Registierung end

---
+++ Frontend/src/components/buttons/SubmitButton.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const SubmitButton = () => {
+  return (
+    <div>
+      <button className="btn bg-button-blue border border-0.5 border-button-blue hover:border-button-orange">
+        Kostenlos Registrieren
+      </button>
+    </div>
+  );
+};
+
+export default SubmitButton;
