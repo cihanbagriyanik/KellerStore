2cb299aa361cb7dd7d39c9eccfc371cc174d9085
cihanbagriyanik <cihanbagriyanikde@gmail.com>
2024-05-16 14:14:08 +0200

    register

--- Frontend/src/components/forms/RegisterForm.jsx
+++ Frontend/src/components/forms/RegisterForm.jsx
@@ -1,20 +1,53 @@
 import React from "react";
 import SubmitButton from "../buttons/SubmitButton";
+import useAuthCall from "../../hooks/useAuthCall";
+import { useState } from "react";
 
 const RegisterForm = () => {
+  const { register } = useAuthCall();
+
+  const [error, setError] = useState(null);
+  const [formValues, setFormValues] = useState({
+    userType: "",
+    profileName: "",
+    email: "",
+    password: "",
+  });
+
+  const handleChange = (e) => {
+    setFormValues({
+      ...formValues,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      await register(formValues); // Send Form informations to register function
+    } catch (err) {
+      setError("Registration failed. Please try again."); // Hata mesajı
+    }
+  };
+
+  console.log(formValues);
+
   return (
     <div className="sm:col-span-12 md:col-span-6 xl:col-span-4 xxl:col-span-3">
-      <form>
+      <form onSubmit={handleSubmit}>
         <div>
-        <h4 className=" text-black text-lg font-semibold text-center m-3">Wie möchtest du Kleinanzeigen nutzen?</h4>
+          <h4 className=" text-black text-lg font-semibold text-center m-3">
+            Wie möchtest du Kleinanzeigen nutzen?
+          </h4>
           <div>
             <div className="flex items-center gap-x-3 ">
-            
               <div className="flex items-center gap-x-3 border-2 border-gray-300 rounded-md p-1 px-7">
                 <input
                   id="privat"
-                  name="privat"
+                  name="userType"
                   type="radio"
+                  value="privat"
+                  onChange={handleChange}
                   className="h-4 w-4 border-gray-300 text-indigo-600 focus:ring-indigo-600"
                 />
                 <label
@@ -27,8 +60,10 @@ const RegisterForm = () => {
               <div className="flex items-center gap-x-3 border-2 border-gray-300 rounded-md p-1 px-7">
                 <input
                   id="gewerblich"
-                  name="gewerblich"
+                  name="userType"
                   type="radio"
+                  value="gewerblich"
+                  onChange={handleChange}
                   className="h-4 w-4 border-gray-300 text-indigo-600 focus:ring-indigo-600"
                 />
                 <label
@@ -47,9 +82,11 @@ const RegisterForm = () => {
               <input
                 id="profileName"
                 name="profileName"
-                type="profileName"
+                type="text"
                 autoComplete="profileName"
                 placeholder="Profil Name"
+                value={formValues.profileName}
+                onChange={handleChange}
                 className=" p-3 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
               />
             </div>
@@ -66,6 +103,8 @@ const RegisterForm = () => {
                 type="email"
                 autoComplete="email"
                 placeholder="email"
+                value={formValues.email}
+                onChange={handleChange}
                 className=" p-3 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400  focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
               />
             </div>
@@ -82,10 +121,15 @@ const RegisterForm = () => {
                 type="password"
                 autoComplete="password"
                 placeholder="Password"
+                value={formValues.password}
+                onChange={handleChange}
                 className=" p-3 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
               />
             </div>
           </div>
+          {error && (
+            <div className="text-red-500 text-center mt-3">{error}</div>
+          )}
           <div className="mt-5 flex justify-center">
             <SubmitButton />
           </div>
