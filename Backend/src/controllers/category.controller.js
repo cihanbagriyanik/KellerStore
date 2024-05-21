559f16658faa6a7d31ecf0f6d3077ea5156cb7e7
Muhammet <msolmaz83@gmail.com>
2024-05-21 21:26:55 +0200

    kist2

--- Backend/src/controllers/category.controller.js
+++ Backend/src/controllers/category.controller.js
@@ -53,7 +53,7 @@ module.exports = {
         `
     */
 
-    const data = await res.find({Category});
+    const data = await Category.find({});
 
     if (data) {
       const categoryList = createCategories(data);
