4ecd70c4f2ff04842e77286f8ca24f404219ff59
Muhammet <msolmaz83@gmail.com>
2024-05-21 21:20:45 +0200

    list

--- Backend/src/controllers/category.controller.js
+++ Backend/src/controllers/category.controller.js
@@ -53,7 +53,7 @@ module.exports = {
         `
     */
 
-    const data = await res.getModelList(Category);
+    const data = await res.find(Category);
 
     if (data) {
       const categoryList = createCategories(data);
@@ -104,7 +104,7 @@ module.exports = {
       if(data){
          res.send({
         message:"okey",
-        data
+        
       })
       }else{
         res.send({message:"data yojk"})
