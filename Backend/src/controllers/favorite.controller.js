b5ed8531dc09b2fe9c60d87212465c000b3c3ffe
Muhammet Solmaz <86296198+Msolmaz4@users.noreply.github.com>
2024-05-25 11:32:48 +0200

    Update favorite.controller.js

--- Backend/src/controllers/favorite.controller.js
+++ Backend/src/controllers/favorite.controller.js
@@ -96,6 +96,27 @@ module.exports = {
       data,
     });
   },
+    belibt: async (req, res) => {
+    /*
+      #swagger.tags = ["Favorites"]
+      #swagger.summary = "Belibts Favorite"
+      #swagger.description = `
+        You can send query with endpoint for search[], sort[], page and limit.
+          <ul> Examples:
+            <li>URL/?<b>search[field1]=value1&search[field2]=value2</b></li>
+            <li>URL/?<b>sort[field1]=1&sort[field2]=-1</b></li>
+            <li>URL/?<b>page=2&limit=1</b></li>
+        </ul>
+      `
+    */
+    const data = await Favorite.find({}).sort(
+      (a, b) => b.favorites.length - a.favorites.length
+    );
+    res.status(200).send({
+      error: false,
+      data,
+    });
+  },
 
   //! /:id -> PUT / PATCH
   update: async (req, res) => {
