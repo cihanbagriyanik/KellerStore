e84546b834e7984b988401f8b7661d713cb7af41
Muhammet Solmaz <86296198+Msolmaz4@users.noreply.github.com>
2024-05-25 12:16:17 +0200

    Update favorite.route.js

--- Backend/src/routers/favorite.route.js
+++ Backend/src/routers/favorite.route.js
@@ -16,8 +16,9 @@ const { isAdmin, isLogin } = require("../middlewares/permissions");
 // router.use(isAdmin);
 router.route("/")
 .get(isAdmin,Favorite.list)
-.route("/belibt").get(Favorite.belibt)
 .post(isLogin, Favorite.create);
+router.route("/belibt").get(Favorite.belibt)
+
 router
   .route("/:id")
   .get(Favorite.read)
