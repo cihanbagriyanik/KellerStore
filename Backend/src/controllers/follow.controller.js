dc17d762b7c6104e806a537a84efc6799cbfa003
Muhammet <msolmaz83@gmail.com>
2024-06-07 09:42:59 +0200

    follow_repeat

--- Backend/src/controllers/follow.controller.js
+++ Backend/src/controllers/follow.controller.js
@@ -65,10 +65,8 @@ module.exports = {
         (item) => item.followUserId == followUserId
       );
       if (followController) {
-        return res.status(400).send({
-          error: true,
-          message: "kullanici var.",
-        });
+        res.errorStatusCode = 401;
+        throw new Error("kullanici var  HATALI.");
       } else {
         const data = await Follow.create(req.body);
         res.status(201).send({
