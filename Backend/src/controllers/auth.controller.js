17bfe4d786376b7219b14276af3e45d911bd1c7a
Muhammet <msolmaz83@gmail.com>
2024-07-04 11:26:04 +0200

    senmail

--- Backend/src/controllers/auth.controller.js
+++ Backend/src/controllers/auth.controller.js
@@ -49,17 +49,17 @@ module.exports = {
         email: req.body.email,
         password: bcrypt.hashSync(req.body.password, 10),
       });
-      sendMail({
-        to: req.body.email,
-        subject: "Welcome",
-        message: `
+       sendMail(
+         req.body.email,
+         "Welcome",
+         `
           <h1>Welcome to the system</h1>
           <p><b>${req.body.userName}</b>, The registration has been successful. Have a nice shopping!</p>
         `,
-      });
+      );
       // Veriyi MongoDB'ye kaydediyoruz.
       const data = await user.save();
-      
+
       res.send({
         data,
         success: true,
